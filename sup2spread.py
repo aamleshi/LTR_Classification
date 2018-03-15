9ca17ac52f3f2358a38e017f339783282cd144aa
aamleshi <aamleshi@gmail.com>
2018-03-15 09:28:00 -0500

    adding scripts

--- sup2spread.py
+++ sup2spread.py
@@ -16,6 +16,7 @@ text = text.replace('i>', '')
 text = text.replace('>p', '')
 raw_proteins = text.split('>')
 ID = []
+superfamily = []
 family = []
 thingy = []
 info = []
@@ -24,13 +25,15 @@ for i in tqdm(np.arange(len(raw_proteins)-1)):
     record = raw_proteins[i+1].split(' ', maxsplit = 1)
     ID.append(record[0])
     record = record[1].split(' ', maxsplit = 1)
-    family.append(record[0])
+    temp = record[0].split('.', maxsplit = 3);
+    superfamily.append(temp[0]+'.'+temp[1]+'.'+temp[2])
+    family.append(temp[3])
     record = record[1].split(' ', maxsplit = 1)
     thingy.append(record[0])
     record = record[1].split('}', maxsplit = 1)
-    info.append(record[0]+ '}')
+    info.append('{' + record[0]+ '}')
     sequence.append(record[1])
-records = [list(a) for a in zip(ID, family, thingy, info, sequence)]
-labels = ["ID", 'Family', 'Thingy', 'Info', 'Sequence']
+records = [list(a) for a in zip(ID, superfamily, family, thingy, info, sequence)]
+labels = ["ID", "Superfamily", 'Family', 'Thingy', 'Info', 'Sequence']
 df =  pd.DataFrame.from_records(records, columns=labels)
 df.to_csv('sequences.csv')
